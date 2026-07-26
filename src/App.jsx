f6aab352a1b0d7ca107cfe434f94c39322c68622
sanjv102 <rai.raisanjeevkumar@gmail.com>
2026-07-26 16:45:53 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <div>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/Sanjeev-Portfolio' element={<Home/>} />
       </Routes>
       <Footer/>
     </div>

061094723bff0abcf6514391ad5f3916f24799d9
sanjv102 <rai.raisanjeevkumar@gmail.com>
2026-07-26 18:10:15 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
+function DiagnosticHome() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center", color: "white" }}>
+      <h1>🎉 Router Connection Successful!</h1>
+      <p>If you can see this text, your GitHub Pages deployment and layout configurations are working flawlessly.</p>
+    </div>
+  );
+}
+
+
 const App = () => {
   return (
     <div>
